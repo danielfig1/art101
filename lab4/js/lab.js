223297e17ff674321b1364df68606de45a229888
danielfig1 <daanfigu@ucsc.edu>
2022-04-18 20:57:25 -0700

    Lab 4: Pseudocoding
    added pseudocode to the lab,js file
    formatted the index.html file to show my work and to be included in my homepage

---
+++ lab4/js/lab.js
@@ -0,0 +1,12 @@
+  // Summary: Lab 4: Problem-solving and Pseudocoding - Flipping a Coin.
+  // Author(s): Daniel Figueroa and Paige Anderson.
+  // Date: 4/18/2022
+
+  //Flipping a coin:
+    // Prompt player with the choices “Heads” or “Tails”.
+    // Accept player input.
+    // Assign the computer with the remaining option.
+    // Print an image of a coin.
+      // When clicked, the program will select a random side to print.
+    // If the coin displays the player’s choice, increment their score by 1.
+    // If the coin displays the computer’s choice, increment its score by 1.
