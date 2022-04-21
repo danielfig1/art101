0bb8b6619387286b7644fd02902413ed938652c2
danielfig1 <daanfigu@ucsc.edu>
2022-04-20 20:53:44 -0700

    lab 5: data types and variables
    added lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,17 @@
+// Summary: Lab 5: Data Types and Variables.
+// Author: Daniel Figueroa.
+// Date: 4/21/2022
+
+// Defining Variables
+make = "Dodge";
+model = "Hellcat";
+color = "Black";
+year = 2016;
+age = 2022 - year
+
+//Outputs
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
